7e96ea37f9340c5680028b9bdfde0ac0aecb107c
mina WAGDY <minaw77@gmail.com>
2023-12-12 15:18:31 +0200

    vue project

---
+++ src/views/Students/StudentView.vue
@@ -0,0 +1,68 @@
+<template>
+   <div>
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          Students
+          <RouterLink to="/student/create" class="btn btn-primary float-end">
+            Add Student
+          </RouterLink>
+        </h4>
+      </div>
+      <div class="card-body">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Course</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Created At</th>
+              <th>Action</th>
+            </tr>
+            </thead>
+           <tbody>
+            <tr>
+
+            </tr>
+           </tbody>
+
+        </table>
+      </div>
+    </div>
+   </div>
+  </template>
+  
+  <script>
+
+  import axios from 'axios'
+
+  export default{
+    name:'students',
+    data(){
+      return{
+        students:[]
+
+      }
+    },
+
+    mounted(){
+      this.getStudents();
+      // console.log('i am here')
+    },
+    methods:{
+
+      getStudents(){
+        axios.get('http://localhost:8000/api/students').then(res => {
+
+          console.log(res)
+
+        });
+      }
+
+    },
+
+  }
+ 
+  </script>
